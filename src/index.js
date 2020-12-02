d21a68d46036299be4472d2bd2964b90a4fa9245
Artem <64410376+Artiss90@users.noreply.github.com>
2020-12-02 17:49:58 +0200

    получаем данные

--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
 // https://www.smashingmagazine.com/2018/01/deferring-lazy-loading-intersection-observer-api/
 
-// import './js/if';
+import './js/if';
 // import './js/io';
-// import articlesTpl from './templates/articles.hbs';
-// import NewsApiService from './js/news-service';
+import articlesTpl from './templates/articles.hbs';
+import NewsApiService from './js/news-service';
 
-// const refs = getRefs();
-// const newsApiService = new NewsApiService();
+const refs = getRefs();
+const newsApiService = new NewsApiService();
 
-// refs.searchForm.addEventListener('submit', onSearch);
+refs.searchForm.addEventListener('submit', onSearch);
 
-// function onSearch(e) {
-//   e.preventDefault();
-//   newsApiService.query = e.currentTarget.elements.query.value;
+function onSearch(e) {
+  e.preventDefault();
+  newsApiService.query = e.currentTarget.elements.query.value;
 
-//   if (newsApiService.query === '') {
-//     return alert('Введи что-то нормальное');
-//   }
+  if (newsApiService.query === '') {
+    return alert('Введи что-то нормальное');
+  }
 
-//   newsApiService.resetPage();
-//   clearArticlesContainer();
-//   newsApiService.fetchArticles().then(articles => {
-//     appendArticlesMarkup(articles);
-//     newsApiService.incrementPage();
-//   });
-// }
+  newsApiService.resetPage();
+  clearArticlesContainer();
+  newsApiService.fetchArticles().then(articles => {
+    appendArticlesMarkup(articles);
+    newsApiService.incrementPage();
+  });
+}
 
-// function appendArticlesMarkup(articles) {
-//   refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
-// }
+function appendArticlesMarkup(articles) {
+  refs.articlesContainer.insertAdjacentHTML('beforeend', articlesTpl(articles));
+}
 
-// function clearArticlesContainer() {
-//   refs.articlesContainer.innerHTML = '';
-// }
+function clearArticlesContainer() {
+  refs.articlesContainer.innerHTML = '';
+}
 
-// function getRefs() {
-//   return {
-//     searchForm: document.querySelector('.js-search-form'),
-//     articlesContainer: document.querySelector('.js-articles-container'),
-//     sentinel: document.querySelector('#sentinel'),
-//   };
-// }
+function getRefs() {
+  return {
+    searchForm: document.querySelector('.js-search-form'),
+    articlesContainer: document.querySelector('.js-articles-container'),
+    sentinel: document.querySelector('#sentinel'),
+  };
+}
 
-// const onEntry = entries => {
-//   entries.forEach(entry => {
-//     if (entry.isIntersecting && newsApiService.query !== '') {
-//       // console.log('Пора грузить еще статьи' + Date.now());
-//       newsApiService.fetchArticles().then(articles => {
-//         appendArticlesMarkup(articles);
-//         newsApiService.incrementPage();
-//       });
-//     }
-//   });
-// };
+const onEntry = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && newsApiService.query !== '') {
+      // console.log('Пора грузить еще статьи' + Date.now());
+      newsApiService.fetchArticles().then(articles => {
+        appendArticlesMarkup(articles);
+        newsApiService.incrementPage();
+      });
+    }
+  });
+};
 
-// const observer = new IntersectionObserver(onEntry, {
-//   rootMargin: '150px',
-// });
-// observer.observe(refs.sentinel);
+const observer = new IntersectionObserver(onEntry, {
+  rootMargin: '150px',
+});
+observer.observe(refs.sentinel);
--- src/js/if.js
+++ src/js/if.js
@@ -1,28 +1,35 @@
-// import InfiniteScroll from 'infinite-scroll';
+import InfiniteScroll from 'infinite-scroll';
 
-// // var unsplashID =
-// //   '9ad80b14098bcead9c7de952435e937cc3723ae61084ba8e729adb642daf0251';
+var unsplashID = '9ad80b14098bcead9c7de952435e937cc3723ae61084ba8e729adb642daf0251';
+// const KEY = 'f08cee847cfc4025a18e3751e639ba81';
 
-// const infScroll = new InfiniteScroll('.container', {
-//   responseType: 'text',
-//   history: false,
-//   path() {
-//     return `https://newsapi.org/v2/everything?q=bitcoin&apiKey=bb47a995514a49758140b073ef1103f5`;
-//   },
-// });
+const infScroll = new InfiniteScroll('.test-container', {
+  responseType: 'text',
+  history: false,
+  path() {
+    //const url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${KEY}`;
+    const url = 'https://api.unsplash.com/photos?client_id=' + unsplashID + '&page=' + this.pageIndex;
+    return url;
+  },
+});
 
-// infScroll.loadNextPage();
+infScroll.loadNextPage();
 
-// infScroll.on('load', (response, path) => {
-//   console.log(JSON.parse(response));
-// });
+setTimeout(() => {
+  infScroll.loadNextPage();
+}, 1000);
+
+infScroll.on('load', (response, path) => {
+  console.log(JSON.parse(response));
+  console.log(path);
+});
 
   // тут по шаблну сделали строку с тегами
   // потом кинули в фрагмент
   // фрагмент передали в infScroll.appendItems(фоагмент)
 
-// const markup = '<p>qweqweqwe</p>';
-// const fragment = new DocumentFragment();
-// const d = document.createElement('div');
-// fragment.innerHTML = markup;
-// console.log(fragment);
+const markup = '<p>qweqweqwe</p>';
+const fragment = new DocumentFragment();
+const d = document.createElement('div');
+fragment.innerHTML = markup;
+console.log(fragment);
